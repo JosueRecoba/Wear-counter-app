8421ebd29f2c188a34844183f918ec5d11713324
Josue Recoba Huerta <josue.huer.reco.22@gmail.com>
2023-07-13 11:28:05 -0600

    Colores dependiendo el valor del intervalo

--- lib/assets/interval_progress_bar.dart
+++ lib/assets/interval_progress_bar.dart
@@ -10,17 +10,30 @@ class IntervalProgressBar extends StatefulWidget {
 }
 
 class _IntervalProgressBarState extends State<IntervalProgressBar> {
-  List<Color> dartColors = [
-    const Color.fromARGB(255, 0, 255, 200),
-    const Color.fromARGB(255, 0, 255, 136),
-    const Color.fromARGB(255, 0, 255, 76),
-    const Color.fromARGB(255, 81, 255, 0),
-    const Color.fromARGB(255, 166, 255, 0),
-    const Color.fromARGB(255, 238, 255, 0),
-    const Color.fromARGB(255, 238, 255, 0),
-    const Color.fromARGB(255, 255, 102, 0),
-    const Color.fromARGB(255, 255, 51, 0),
-    const Color.fromARGB(255, 255, 0, 0),
+    int get value => widget.value;
+    List<Color> darkColors = [
+      const Color.fromARGB(255, 0, 150, 117),
+      const Color.fromARGB(255, 0, 151, 81),
+      const Color.fromARGB(255, 0, 148, 44),
+      const Color.fromARGB(255, 45, 141, 0),
+      const Color.fromARGB(255, 95, 146, 0),
+      const Color.fromARGB(255, 132, 141, 0),
+      const Color.fromARGB(255, 139, 139, 0),
+      const Color.fromARGB(255, 145, 99, 0),
+      const Color.fromARGB(255, 138, 55, 0),
+      const Color.fromARGB(255, 145, 0, 0),
+    ];
+    List<Color> brightColors = [
+      const Color.fromARGB(255, 0, 255, 200),
+      const Color.fromARGB(255, 0, 255, 136),
+      const Color.fromARGB(255, 0, 255, 76),
+      const Color.fromARGB(255, 81, 255, 0),
+      const Color.fromARGB(255, 166, 255, 0),
+      const Color.fromARGB(255, 238, 255, 0),
+      const Color.fromARGB(255, 255, 255, 0),
+      const Color.fromARGB(255, 255, 174, 0),
+      const Color.fromARGB(255, 255, 102, 0),
+      const Color.fromARGB(255, 255, 0, 0),
   ];
       // ignore: prefer_final_fields
       double _progress = 1.0;
@@ -40,31 +53,39 @@ class _IntervalProgressBarState extends State<IntervalProgressBar> {
   }
 
   Widget _intervalBar(){
+        List<Color> actualColors;
+
+    if(value == 0){
+      actualColors = darkColors;
+    }else{
+      actualColors = brightColors;
+    }
+
     return  Column(
       children: [
-        _interval(customColor: const Color.fromARGB(255, 0, 255, 200)),
-        const SizedBox(height: 2.0,),
-        _interval(customColor: const Color.fromARGB(255, 0, 255, 136)),
-        const SizedBox(height: 2.0,),
-        _interval(customColor: const Color.fromARGB(255, 0, 255, 76)),
-        const SizedBox(height: 2.0,),
-        _interval(customColor: const Color.fromARGB(255, 81, 255, 0)),
-        const SizedBox(height: 2.0,),
-        _interval(customColor: const Color.fromARGB(255, 166, 255, 0)),
-        const SizedBox(height: 2.0,),
-        _interval(customColor: const Color.fromARGB(255, 238, 255, 0)),
-        const SizedBox(height: 2.0,),
-        _interval(customColor: const Color.fromARGB(255, 238, 255, 0)),
-        const SizedBox(height: 2.0,),
-        _interval(customColor: const Color.fromARGB(255, 255, 102, 0)),
-        const SizedBox(height: 2.0,),
-        _interval(customColor: const Color.fromARGB(255, 255, 51, 0)),
-        const SizedBox(height: 2.0,),
-        _interval(customColor: const Color.fromARGB(255, 255, 0, 0)),
+        _interval(customColor: actualColors[0] ),
+        _separator(),
+        _interval(customColor: actualColors[1]),
+        _separator(),
+        _interval(customColor: actualColors[2]),
+        _separator(),
+        _interval(customColor: actualColors[3]),
+        _separator(), 
+        _interval(customColor: actualColors[4]),
+        _separator(),
+        _interval(customColor: actualColors[5]),
+        _separator(),
+        _interval(customColor: actualColors[6]),
+        _separator(),
+        _interval(customColor: actualColors[7]),
+        _separator(),
+        _interval(customColor: actualColors[8]),
+        _separator(),
+        _interval(customColor: actualColors[9]),
+
 
         const SizedBox(width: 35.0,),
       ],
-
     );
   }
   
@@ -72,6 +93,12 @@ class _IntervalProgressBarState extends State<IntervalProgressBar> {
     return Text(
       _progress.toStringAsFixed(1),
     );
+  }
+
+  Widget _separator(){
+    return const SizedBox(
+          height: 2.2,
+        );
   }
 
   Widget _interval({required Color customColor}){
